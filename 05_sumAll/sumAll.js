5cb1892ae42cfb91b22fdffbc2735bd6be6338c4
Mercedes Wiesztort <mercedesw13@protonmail.com>
2022-08-27 19:30:34 -0300

    resuelve ej 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (a < 0 || b < 0) return "ERROR";
+    if (typeof a !== 'number' || typeof b !== 'number') return "ERROR";
+
+    const first = a > b ? b : a;
+    const last = b > a ? b : a;
+    let sum = 0;
+
+    for (let i = first; i <= last; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
