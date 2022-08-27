a84ac0e6d28c4c50b6ef7eec93f80dfac92ea083
Mercedes Wiesztort <mercedesw13@protonmail.com>
2022-08-27 19:29:41 -0300

    resuelve ej 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    /* const arr1 = str.split(' ');
+    let arr2 = [];
+
+    for (const s of arr1) {
+        const l = s.split('').reverse().join('');
+        arr2.push(l);
+    }
+
+    const result = arr2.reverse().join(' ') */
+
+    return str.split('').reverse().join('');
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
